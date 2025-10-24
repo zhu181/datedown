7c6d324dcb58346e6f9f2629adfe26ffdc15fa3e
Zhu Liyang <36814419+zhu181@users.noreply.github.com>
2025-10-24 08:32:09 +0800

    refactor: improve formatting and structure of setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-    Setup file for datedown.
-    Use setup.cfg to configure your project.
+Setup file for datedown.
+Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
+This file was generated with PyScaffold 3.3.
+PyScaffold helps you to put up the scaffold of your new Python project.
+Learn more under: https://pyscaffold.org/
 """
 import sys
-from importlib.metadata import version
+
 
 from setuptools import setup
 
-if version("setuptools") < "38.3":
+version = None
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from pkg_resources import VersionConflict, require
+
+    try:
+        require("setuptools>=38.3")
+    except VersionConflict:
+        print("Error: version of setuptools is too old (<38.3)!")
+        sys.exit(1)
+
+if version is not None and version("setuptools") < "38.3":
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
